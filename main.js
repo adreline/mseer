0612dbba1b1d8c5f531d0c5f77297cbd7b344394
Ubuntu <maddie.madlad@gmail.com>
2022-09-03 16:32:55 +0200

    global process var and first start

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const { Frontend } = require('./frontend/frontend.js');
+const fs = require('fs');
 const path = require('path');
+const H = require('./modules/aide.js');
 global.documentRoot = path.resolve(__dirname);
+global.Spools = {};
+//on setup, clear 'online' status of all servers
+H.getMaster()
+    .then(master => {
+        for (const uid in master) {
+            console.log(`clearing ${uid}`);
+            master[uid].pid = '-';
+        }
+        fs.writeFileSync(`${documentRoot}/mojang/master.json`, JSON.stringify(master));
+    })
+    .then(() => {
+        const webInterface = new Frontend();
+        webInterface.serve();
+    })
 
-const webInterface = new Frontend();
-
-webInterface.serve();
