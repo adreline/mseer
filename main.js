51252b4fe54c8c911b1ca4db0da4dbe523016e6c
Ubuntu <maddie.madlad@gmail.com>
2022-09-03 17:29:54 +0200

    create dir tree on first boot

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const path = require('path');
 const H = require('./modules/aide.js');
 global.documentRoot = path.resolve(__dirname);
 global.Spools = {};
+//verify if directory tree is intact 
+if(!fs.existsSync(`${documentRoot}/mojang`)){
+    console.log('Creating directory tree');
+    fs.mkdirSync(`${documentRoot}/mojang`);
+    fs.mkdirSync(`${documentRoot}/mojang/bins`);
+    fs.writeFileSync(`${documentRoot}/mojang/master.json`,'{}');
+}
 //on setup, clear 'online' status of all servers
 H.getMaster()
     .then(master => {
